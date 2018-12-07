8d992078f3fa5163c575d412fe7fe9e73e625272
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-12-07 13:19:53 +0530

    Limited Player Slope Control

---
+++ Assets/Scripts/Player/Movement/ControlSlopeClimb.cs
@@ -0,0 +1,40 @@
+﻿using AstroWorld.Extras;
+using UnityEngine;
+
+namespace AstroWorld.Player.Movement
+{
+    public class ControlSlopeClimb : MonoBehaviour
+    {
+        public Transform raycastPoint;
+        public float maxDistance;
+        [Range(0, 360)]
+        public float maxAngleAllowed;
+
+        private RaycastHit _hit;
+        private Rigidbody _playerRB;
+
+        /// <summary>
+        /// Start is called on the frame when a script is enabled just before
+        /// any of the Update methods is called the first time.
+        /// </summary>
+        void Start() => _playerRB = GetComponent<Rigidbody>();
+
+        /// <summary>
+        /// LateUpdate is called every frame, if the Behaviour is enabled.
+        /// It is called after all Update functions have been called.
+        /// </summary>
+        void LateUpdate() => StopPlayerOnExtremeSlope();
+
+        private void StopPlayerOnExtremeSlope()
+        {
+            if (Physics.Raycast(raycastPoint.position, Vector3.down, out _hit, maxDistance))
+            {
+                float angle = Vector3.Angle(_hit.normal, transform.forward);
+                float normalizedAngle = ExtensionFunctions.To360Angle(angle);
+
+                if (normalizedAngle - 90 > maxAngleAllowed)
+                    _playerRB.velocity = Vector3.zero;
+            }
+        }
+    }
+}
