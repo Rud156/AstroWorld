71421d77706a25cffcf2972e8865a788bad2685f
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-12-09 09:59:25 +0530

    Adding Enemy Response System

---
+++ Assets/Scripts/Enemies/Creature/AlertNearbyEnemiesOnDead.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AstroWorld.Common;
+using UnityEngine;
+
+namespace AstroWorld.Enemies.Creature
+{
+    [RequireComponent(typeof(HealthSetter))]
+    [RequireComponent(typeof(DamageAmount))]
+    public class AlertNearbyEnemiesOnDead : MonoBehaviour
+    {
+        public GameObject battery;
+        public float enemyRangeToTarget;
+
+        private DamageAmount _damageAmount;
+        private HealthSetter _healthSetter;
+
+        /// <summary>
+        /// Start is called on the frame when a script is enabled just before
+        /// any of the Update methods is called the first time.
+        /// </summary>
+        void Start()
+        {
+            _damageAmount = GetComponent<DamageAmount>();
+
+            _healthSetter = GetComponent<HealthSetter>();
+            _healthSetter.healthZero += OnEnemyDead;
+        }
+
+        private void OnEnemyDead()
+        {
+            Instantiate(battery, transform.position, Quaternion.identity);
+            Collider[] colliders = Physics.OverlapSphere(transform.position, enemyRangeToTarget);
+
+            foreach (Collider collider in colliders)
+            {
+                HealthSetter healthSetter = collider.GetComponent<HealthSetter>();
+                if (healthSetter != null)
+                    healthSetter.ReduceHealth(_damageAmount.damageAmount);
+
+                CreaturePatrol creaturePatrol = collider.GetComponent<CreaturePatrol>();
+                if (creaturePatrol != null)
+                    creaturePatrol.SetPlayerHostile();
+            }
+        }
+    }
+}
